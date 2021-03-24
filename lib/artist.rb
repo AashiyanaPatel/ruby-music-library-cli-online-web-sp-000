444edfab522121a725ebb4e22abafb62483fc528
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-24 00:30:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,55 +1,40 @@
 class Artist
   extend Concerns::Findable
+
   attr_accessor :name
-  attr_reader :songs
 
   @@all = []
 
   def initialize(name)
     @name = name
-    @songs = []
-  end
-
-  def songs
-    @songs
   end
 
   def self.all
     @@all
   end
 
+  def self.destroy_all
+    all.clear
+  end
+
   def self.create(name)
-    artist = self.new(name)
-    artist.save
-    artist
+    new(name).save
   end
 
   def save
-    @@all << self
-  end
-
-  def self.destroy_all
-    @@all.clear
+    self.class.all << self
+    self
   end
 
   def add_song(song)
-    if song.artist == self
-      song
-    else
-      song.artist = self
-    end
-    if self.songs.detect {|song_in_array| song_in_array == song}
-      song
-    else
-      self.songs << song
-    end
-    song
+    song.artist = self unless song.artist
+  end
+
+  def songs 
+    Song.all.select { |s| s.artist == self }
   end
 
   def genres
-    genres = @songs.collect do |song|
-      song.genre
-    end
-    genres.uniq
+    songs.collect{ |s| s.genre }.uniq
   end
 end
