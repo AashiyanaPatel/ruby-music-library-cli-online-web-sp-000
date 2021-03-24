776f4d3d6f07056d14288983bc30e97c71ceded0
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-24 00:24:08 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,7 @@
 module Concerns::Findable
   
     def find_by_name(name)
-      self.all.detect do{|s| s.name == name}
+      self.all.detect do {|s| s.name == name}
     end
       
     def find_or_create_by_name(name)
